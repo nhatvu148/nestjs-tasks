7347f079b24fca0f1c0c7b0a4b5dc4572b2b72be
nhatvu148 <nhatvu148@gmail.com>
2020-11-15 01:22:35 +0900

    post createTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,8 +11,15 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  //   @Post()
+  //   createTask(@Body() body) {
+  //     console.log("body", body);
+  //   }
   @Post()
-  createTask(@Body() body) {
-    console.log("body", body);
+  createTask(
+    @Body("title") title: string,
+    @Body("description") description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
   }
 }
