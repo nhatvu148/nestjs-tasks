2160abd769d2f2d1252b26ff1ad48b5055d40ac4
nhatvu148 <nhatvu148@gmail.com>
2020-11-15 00:50:28 +0900

    use prettier

---
+++ PRETTIERCMD.md
@@ -0,0 +1,10 @@
+## Prettier Commands
+
+- yarn add --dev --exact prettier
+
+- Create empty .prettierrc.json to let editors and other tooling know you are using Prettier.
+
+- yarn prettier --write .
+- yarn prettier --write app/
+- yarn prettier --write app/components/Button.js
+- yarn prettier --check .
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {}
 
-    @Get()
-    getAllTasks() {
-        return this.tasksService.getAllTasks();
-    }
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
 }
